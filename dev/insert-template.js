a0b298252e104ee0eb77670b4ce55efbc03c27fd
Michael Marcenich <michael.marcenich@gmail.com>
2018-11-01 16:45:27 +0100

    fix(loading): include css staticly

--- dev/insert-template.js
+++ dev/insert-template.js
@@ -5,11 +5,16 @@ const input = fs.readFileSync(
     path.join(__dirname, 'dist/WebpackAssets.typoscript'),
     'utf8'
 );
-const [source] = input.match(/<div id="webpack-plugin-loader">[\s\S]+<\/div>/);
+const [template] = input.match(
+    /<div id="webpack-plugin-loader">[\s\S]+<\/div>/
+);
+const [, style] = input.match(/<style type="text\/css">([\s\S]*)<\/style>/);
 
 const origin = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-const output = origin.replace(
-    /<div id="webpack-plugin-loader">[\s\S]*<\/div>/g,
-    source
-);
+const output = origin
+    .replace(/<div id="webpack-plugin-loader">[\s\S]*<\/div>/g, template)
+    .replace(
+        /(<style id="webpack-plugin-style">)[\s\S]*(<\/style>)/g,
+        `$1${style}$2`
+    );
 fs.writeFileSync(path.join(__dirname, 'index.html'), output);
